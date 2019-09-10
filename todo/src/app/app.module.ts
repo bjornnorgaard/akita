a83efeac13752cc91c2097efc425f29c636dd57e
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2019-09-10 21:51:22 +0200

    Setup angular material

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InputComponent } from './components/input/input.component';
-import { CompletedComponent } from './components/completed/completed.component';
-import { TodosComponent } from './components/todos/todos.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {InputComponent} from './components/input/input.component';
+import {CompletedComponent} from './components/completed/completed.component';
+import {TodosComponent} from './components/todos/todos.component';
+import {NgMaterialModule} from './ng-material/ng-material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { TodosComponent } from './components/todos/todos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
