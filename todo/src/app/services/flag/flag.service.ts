4ebdebe04ca2e7fb77e73f0210f9f93f002d1274
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2019-09-18 22:01:29 +0200

    Added feature flag for auth

--- todo/src/app/services/flag/flag.service.ts
+++ todo/src/app/services/flag/flag.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core';
 import bulletTrain from 'bullet-train-client';
 import {environment} from '../../../environments/environment';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class FlagService {
 
-  constructor() {
+  constructor(private auth: AuthService) {
     bulletTrain.init({
       environmentID: environment.bulletId,
       onChange: (old, params) => {
