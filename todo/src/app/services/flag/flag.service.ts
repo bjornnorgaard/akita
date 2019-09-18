8a2a0b1d508e1749e26fcda3755914439fff9c5f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2019-09-18 22:09:32 +0200

    Added identify to flags

--- todo/src/app/services/flag/flag.service.ts
+++ todo/src/app/services/flag/flag.service.ts
@@ -7,10 +7,16 @@ import {AuthService} from '../auth/auth.service';
 export class FlagService {
 
   constructor(private auth: AuthService) {
+    this.auth.getUser$().subscribe(user => {
+      if (!user) {
+        return;
+      }
+      bulletTrain.identify(user.email);
+    });
     bulletTrain.init({
       environmentID: environment.bulletId,
       onChange: (old, params) => {
-        console.log('OnChange', old, params);
+        // console.log('OnChange', old, params);
       }
     });
   }
